e527f5552fb58d78768fb218f828e71c5b234a6d
Valentin <vtroitino@uade.edu.ar>
2024-10-08 14:04:18 -0300

    Agrego header y pagina de avances

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    colors: {
-      black: "#1c1c19",
-      white: "#fafaf9",
+    extend: {
+      colors: {
+        black: "#1c1c19",
+        white: "#fafaf9",
+      },
+      fontFamily: {
+        title: ["RampartOne", "Roboto", "system-ui"],
+        montserrat: ["Montserrat", "system-ui"],
+      },
+      fontSize: {
+        "h1-desktop": ["7rem", { lineHeight: "7rem" }],
+        "h2-desktop": ["3.5rem", { lineHeight: "3.5rem", fontWeight: "600" }],
+        "p-desktop": ["1.5rem", { lineHeight: "1.5rem", fontWeight: "400" }],
+      },
     },
-    fontFamily: {
-      title: ["RampartOne", "Roboto", "system-ui"],
-      montserrat: ["Montserrat", "system-ui"],
-    },
-    fontSize: {
-      "h1-desktop": ["7rem", { lineHeight: "7rem" }],
-      "h2-desktop": ["3.5rem", { lineHeight: "3.5rem", fontWeight: "600" }],
-    },
-    extend: {},
   },
   plugins: [],
 };
