cd20db22a043494c47926519beb0b2dc4b3a78bb
vtroitio <valentinbtroitino@gmail.com>
2024-10-16 15:53:57 -0300

    Update footer and typography styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
       fontSize: {
         "h1-desktop": ["7rem", { lineHeight: "7rem" }],
         "h2-desktop": ["3.5rem", { lineHeight: "3.5rem", fontWeight: "600" }],
+        "h3-desktop": ["2rem", { lineHeight: "2rem", fontWeight: "600" }],
         "p-desktop": ["1.5rem", { lineHeight: "1.5rem", fontWeight: "400" }],
         "p-sm-desktop": ["1rem", { lineHeight: "1rem", fontWeight: "400" }],
       },
